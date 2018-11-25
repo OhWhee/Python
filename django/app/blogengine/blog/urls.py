47f8cf15dcb959640d31cafbbe7ba6fa36c0874d
OhWhee <42094630+OhWhee@users.noreply.github.com>
2018-11-25 21:09:14 +0300

    k

--- django/app/blogengine/blog/urls.py
+++ django/app/blogengine/blog/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', posts_list, name='posts_list_url'),
-    path('posts/<str:slug>/', post_detail, name='post_detail_url')
+    path('posts/<str:slug>/', post_detail, name='post_detail_url'),
+    path('tags/', tag_list, name='tag_list_url'),
+    path('tag/<str:slug>/', tag_detail, name='tag_detail_url')
 ]
