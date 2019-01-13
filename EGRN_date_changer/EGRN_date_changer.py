814f6ae7e99155464e7110f0862448d4ff6fe2c2
OhWhee <42094630+OhWhee@users.noreply.github.com>
2019-01-13 19:02:58 +0300

    komit

--- EGRN_date_changer/EGRN_date_changer.py
+++ EGRN_date_changer/EGRN_date_changer.py
@@ -1,75 +1,102 @@
 from lxml import etree as ET
-
-def read_xml(path):
-    """Возваращает объект типа xml.etree.ElementTree"""
-    #ET.register_namespace("", 'urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1')
-    ET.register_namespace('smev', 'urn://x-artefacts-smev-gov-ru/supplementary/commons/1.0.1')
-    ET.register_namespace('num', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/numbers/1.0')
-    ET.register_namespace('adrs', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1')
-    ET.register_namespace('spa', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/entity-spatial/5.0.1')
-    ET.register_namespace('param', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/parameters-oks/2.0.1')
-    ET.register_namespace('cer', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/certification-doc/1.0')
-    ET.register_namespace('doc', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/document-output/4.0.1')
-    ET.register_namespace('flat', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/assignation-flat/1.0.1')
-    ET.register_namespace('ch', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/cultural-heritage/2.0.1')
-    tree = ET.parse(path)
-    root = tree.getroot()
-    return root
+import csv
+from datetime import datetime
 
 
-root = read_xml(r'D:\EGRN_date_changer\б-р. Комарова, д. 20, кв. 59 (2).xml')
+class OwnerList:
+    
+    savePath = r'D:\EGRN_date_changer\test.csv'
+    
+    def __init__(self, path):
+        self.path = path
+        self.root = self.read_xml(self.path)
+        self.address = self.get_address(self.root)
+        self.owners = self.get_owners(self.root)
+        
+        
+        
+    def read_xml(self, path):
+        """Возваращает объект типа xml.etree.ElementTree"""
+        ET.register_namespace('smev', 'urn://x-artefacts-smev-gov-ru/supplementary/commons/1.0.1')
+        ET.register_namespace('num', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/numbers/1.0')
+        ET.register_namespace('adrs', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1')
+        ET.register_namespace('spa', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/entity-spatial/5.0.1')
+        ET.register_namespace('param', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/parameters-oks/2.0.1')
+        ET.register_namespace('cer', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/certification-doc/1.0')
+        ET.register_namespace('doc', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/document-output/4.0.1')
+        ET.register_namespace('flat', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/assignation-flat/1.0.1')
+        ET.register_namespace('ch', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/cultural-heritage/2.0.1')
+        tree = ET.parse(path)
+        root = tree.getroot()
+        return root
 
-def get_address(root):
-    street = ""
-    street_type = ""
-    house_type = ""
-    house_number = ""
-    flat_type = ""
-    flat_number = ""
-     
-    for t in root.findall('.//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}Address'):
-        for i in t.getchildren():
-            if i.tag == "{urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1}Street":
-                street, street_type = i.attrib["Name"], i.attrib["Type"]
-            if i.tag == "{urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1}Level1":
-                house_type, house_number = i.attrib["Type"], i.attrib["Value"]
-            if i.tag == "{urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1}Apartment":
-                flat_type, flat_number = i.attrib["Type"], i.attrib["Value"]
-        return r'{}. {}, {}. {}, {}. {}'.format(street_type, street, house_type, house_number, flat_type, flat_number)
- 
-get_address(root)
 
-def replaceDate(root, date):
-    for t in root.findall(".//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}ReestrExtract"):
-        for i in t.getchildren():
-            if i.tag == '{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}DeclarAttribute':
-                print(i.attrib['ExtractDate'], i.attrib['RequeryDate'])
-                i.attrib['ExtractDate'] = date
-                i.attrib['RequeryDate'] = date
-                print(i.attrib['ExtractDate'], i.attrib['RequeryDate'])
-    for t in root.findall(".//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}HeadContent"):
+    def get_address(self, root):
+        street = ""
+        street_type = ""
+        house_type = ""
+        house_number = ""
+        flat_type = ""
+        flat_number = ""
+         
+        for t in root.findall('.//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}Address'):
             for i in t.getchildren():
-                if i.tag == '{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}Content':
-                    i.text = 'На основании запроса от {} г., поступившего на рассмотрение {} г., сообщаем, что согласно записям Единого государственного реестра недвижимости:'.format(date, date)
-    
+                if i.tag == "{urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1}Street":
+                    street, street_type = i.attrib["Name"], i.attrib["Type"]
+                if i.tag == "{urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1}Level1":
+                    house_type, house_number = i.attrib["Type"], i.attrib["Value"]
+                if i.tag == "{urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1}Apartment":
+                    flat_type, flat_number = i.attrib["Type"], i.attrib["Value"]
+            return r'{}. {}, {}. {}, {}. {}'.format(street_type, street, house_type, house_number, flat_type, flat_number)
 
-addr = 'D:/EGRN_date_changer/б-р. Комарова, д. 20, кв. 59 (2).xml'
-date = '15.12.2018'
 
-def getInfo(addr, date):
-    ET.register_namespace('smev', 'urn://x-artefacts-smev-gov-ru/supplementary/commons/1.0.1')
-    ET.register_namespace('num', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/numbers/1.0')
-    ET.register_namespace('adrs', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/address-output/4.0.1')
-    ET.register_namespace('spa', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/entity-spatial/5.0.1')
-    ET.register_namespace('param', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/parameters-oks/2.0.1')
-    ET.register_namespace('cer', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/certification-doc/1.0')
-    ET.register_namespace('doc', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/document-output/4.0.1')
-    ET.register_namespace('flat', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/assignation-flat/1.0.1')
-    ET.register_namespace('ch', 'urn://x-artefacts-rosreestr-ru/commons/complex-types/cultural-heritage/2.0.1')
-    tree = ET.parse(addr)
-    root = tree.getroot()
-    replaceDate(root, date)
-    tree.write(addr, encoding='UTF-8', xml_declaration=True)
+    def replaceDate(self, root, date):
+        for t in root.findall(".//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}ReestrExtract"):
+            for i in t.getchildren():
+                if i.tag == '{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}DeclarAttribute':
+                    print(i.attrib['ExtractDate'], i.attrib['RequeryDate'])
+                    i.attrib['ExtractDate'] = date
+                    i.attrib['RequeryDate'] = date
+                    print(i.attrib['ExtractDate'], i.attrib['RequeryDate'])
+        for t in root.findall(".//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}HeadContent"):
+                for i in t.getchildren():
+                    if i.tag == '{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}Content':
+                        i.text = 'На основании запроса от {} г., поступившего на рассмотрение {} г., сообщаем, что согласно записям Единого государственного реестра недвижимости:'.format(date, date)
+
+
+    def get_owners(self, root):
+        owners = []
+        try:
+            for t in root.findall(".//{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}Person"):
+                for i in t.getchildren():
+                    if i.tag == '{urn://x-artefacts-rosreestr-ru/outgoing/kpoks/4.0.1}Content':
+                        owners.append(i.text)
+            return owners
+        except:
+            owners.append("Отсутствует в выписке")
+            
+    def saveToCSV(self):
+        string_to_csv = []
+        string_to_csv.append(self.address)
+        for string in self.owners:
+            string_to_csv.append(string)
+        with open(self.savePath, 'a', newline='') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';')
+            writer.writerow(string_to_csv)
+        
+
+
+
+files = ['D:/EGRN_date_changer/б-р. Комарова, д. 20, кв. 59 (2).xml', 
+         'D:/EGRN_date_changer/Беляева 13_58 кв. 14.xml',
+         'D:/EGRN_date_changer/пер. Оружейный, д. 11-2, кв. 91 (2).xml']
+
+objs = []
+for file in files:
+    objs.append(OwnerList(file))
     
-getInfo(addr, date)
-replaceDate(root, '15.12.2018')
+    
+test = OwnerList('D:/EGRN_date_changer/Беляева 13_58 кв. 14.xml')
+
+for obj in objs:
+    obj.saveToCSV()
